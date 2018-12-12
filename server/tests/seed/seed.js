a8aca5b8b33def91273266356bcd8b253e378d99
Jeff Yang <jeffyang@Jeffs-MacBook-Pro-4.local>
2018-12-11 21:35:05 -0600

    make todos private

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -19,7 +19,11 @@ const users = [{
 }, {
   _id: userTwoId,
   email: "jeffyang888@gmail.com",
-  password: "userTwoPass"
+  password: "userTwoPass",
+  tokens: [{
+    access: "auth",
+    token: jwt.sign({_id: userTwoId, access: "auth"}, "abc123").toString()
+  }]
 }];
 
 
